9c1050eb9cf7b0c3f6c188bd24c59286b7b30029
sandeep <sandeep.i.navale@intel.com>
2019-11-07 15:01:13 +0530

    updated the routes code for something change

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,11 +3,16 @@ import {withRouter} from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
-    <div className={`${size} menu-item`} onclick={() =>history.push(`${match.url}${linkUrl}`)}
-
-        >
-        <div className='background-image'style={{backgroundImage: `url(${imageUrl})`}} ></div>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+    <div 
+    className={`${size} menu-item`} 
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+        <div className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }} >
+        </div>
         <div className='content'>
           <h1 className='title'>{title.toUpperCase()}</h1>
           <span className='subtitle'>SHOP NOW</span>
