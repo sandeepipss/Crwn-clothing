99526761f1e8e0b842511b614fba594e40bbcbb0
sandeep <sandeep.i.navale@intel.com>
2019-11-07 14:42:31 +0530

    updated the routes code for something

--- src/components/directory/directory.componet.jsx
+++ src/components/directory/directory.componet.jsx
@@ -37,7 +37,7 @@ class Directory extends React.Component{
                   imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                   size: 'large',
                   id: 5,
-                  linkUrl: 'shop/mens'
+                  linkUrl: 'shop/men'
                 }
             ]
         };
@@ -47,8 +47,8 @@ class Directory extends React.Component{
         return (
             <div className='directory-menu'>
             {
-                this.state.sections.map(({title, imageUrl,id, size}) =>
-                    <MenuItem key={id} title ={title} imageUrl={imageUrl} size={size}/>
+                this.state.sections.map(({id, ...otherSectionProps }) =>
+                    <MenuItem key={id} {...otherSectionProps}/>
                     )
             }
             </div>
