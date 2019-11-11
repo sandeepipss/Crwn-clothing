c08900bdb09f5228f7860b2e882bff94dfe52e9a
sandeep <sandeep.i.navale@intel.com>
2019-11-11 15:04:14 +0530

    user added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route,Redirect} from 'react-router-dom';
 import './App.css';
 
 import {connect } from 'react-redux';
@@ -53,7 +53,17 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
-          <Route path='/signin' component={SignInAndSingUpPage}/>
+          <Route 
+            exact 
+            path='/signin' 
+            render={() => 
+              this.props.currentUser ? (
+                <Redirect to='/' />
+                ) : (
+                  <SignInAndSingUpPage/>
+                )
+                }
+            />
         </Switch>
       </div>
     );
@@ -61,9 +71,16 @@ class App extends React.Component {
   
   }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
   
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps 
+)(App);
