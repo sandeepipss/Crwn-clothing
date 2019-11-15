af358674e193b3ef254dec37be0907524f5a1340
sandeep <sandeep.i.navale@intel.com>
2019-11-15 14:47:10 +0530

    added the additem and remove item code in the checkout page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -9,7 +9,23 @@ export  const addItemToCart = (cartItem, cartItemToAdd) => {
             cartItem.id === cartItemToAdd.id
             ? {...cartItem, quantity: cartItem.quantity+1 }
             :cartItem
-            )
+            );
         }
     return [...cartItem, {...cartItemToAdd, quantity:1 }]
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    );
+
+    if ( existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+    return cartItems.map( cartItem =>
+        cartItem.id === cartItemToRemove.id 
+        ? {...cartItem, quantity: cartItem.quantity-1}
+        :cartItem
+    );
+
 };
