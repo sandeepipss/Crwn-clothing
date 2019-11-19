5506188a7b13435145a5683ab6b3b8d01df9a139
sandeep <sandeep.i.navale@intel.com>
2019-11-19 13:17:10 +0530

    updated the page link url and the respective seletors

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,32 +1,19 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
+import {Route} from 'react-router-dom';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+import ColletionPage from '../collection/collection.component';
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-              
-        };
-
-    }
-
-    render(){
-      const { collections } = this.state;
-      return (
-        <div className='shop-page'>
-          {collections.map(({id, ...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-      ) ;
-    }
-}
+import CollectionsOverview from '../../components/collections-overview/collections-overview.componenet';
 
 
+
+const ShopPage =({match}) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+    <Route path = {`${match.path}/:collectionId`} component= {ColletionPage}/>
+  </div>
+) ;
+    
 export default ShopPage;
 
 
