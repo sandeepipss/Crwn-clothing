db5ba9886840ae814f9f97656c9e8a263eeb2338
sandeep <sandeep.i.navale@intel.com>
2019-11-19 14:57:13 +0530

    updated the strip and title for the project

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,9 @@ import {createStructuredSelector} from 'reselect';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
+
+
 import './checkout.styles.scss';
 
 const CheckoutPage =({cartItems, total}) =>(
@@ -32,9 +35,13 @@ const CheckoutPage =({cartItems, total}) =>(
     }
    
 
-    <div className='total'>
-        <span>TOTAL: ${total}</span>
+    <div className='total'>TOTAL: ${total}</div>
+    <div className='test-warning'>
+    *Please use the following test credit card for payments*
+    <br/>
+    4242 4242 4242 4242 - Exp:01/20 - CVV:123
     </div>
+    <StripeCheckoutButton price={total}/>
 </div>
 );
 
